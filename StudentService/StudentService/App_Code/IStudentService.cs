b998b2c1af485646cb2ca712c588cff42a70481c
KirillZorya14 <kirill.villa2016@gmail.com>
2024-12-01 12:42:14 +0100

    Added code for Lab2

---
+++ StudentService/StudentService/App_Code/IStudentService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+[ServiceContract]
+public interface IStudentService
+{
+    [OperationContract]
+    [WebGet(UriTemplate = "/GetStudents?minGrade={minGrade}&maxGrade={maxGrade}", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+    List<Student> GetStudents(string minGrade, string maxGrade);
+}
+
+[DataContract]
+public class Student
+{
+    [DataMember]
+    public string FirstName { get; set; }
+
+    [DataMember]
+    public string LastName { get; set; }
+
+    [DataMember]
+    public string Patronymic { get; set; }
+
+    [DataMember]
+    public float AverageGrade { get; set; }
+
+    public Student(string firstName, string lastName, string patronymic, float averageGrade)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Patronymic = patronymic;
+        this.AverageGrade = averageGrade;
+    }
+}
---
+++ StudentService/StudentService/App_Code/StudentService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Hosting;
+using System.Xml.Linq;
+
+public class StudentService : IStudentService
+{
+    private static readonly string XmlPath = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data", "students.xml");
+
+    public List<Student> GetStudents(string minGrade, string maxGrade)
+    {
+        float min = float.Parse(minGrade);
+        float max = float.Parse(maxGrade);
+        XDocument xdocument = XDocument.Load(XmlPath);
+
+        List<Student> result = xdocument.Root.Elements()
+            .Where(student =>
+            {
+                float avg = float.Parse(student.Element("averageGrade").Value);
+                return avg >= min && avg <= max;
+            })
+            .Select(student => new Student(
+                student.Element("name").Value,
+                student.Element("surname").Value,
+                student.Element("patronymic").Value,
+                float.Parse(student.Element("averageGrade").Value)
+            ))
+            .ToList();
+
+        return result;
+    }
+
+}
